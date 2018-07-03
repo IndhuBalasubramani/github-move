7ccf489ab321cdb039332d1fadd20fd6f10ab7b2
CO25362 <CO25362@HFDVDI019240.ad1.prod>
2018-07-03 07:25:09 -0400

    first commit

---
+++ Hello World/src/Welcome.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Welcome {
+ public static void main(String args[])
+ {
+	 System.out.println("Hello World!");
+	 
+	 System.out.println("Welcome");
+	 
+ }
+}
